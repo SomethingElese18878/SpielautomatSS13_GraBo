b3fdbd590a80133a5012a96228c13be8043d0c96
Norman-Desktop <norman.walker87@gmail.com>
2013-06-22 13:08:10 +0200

    add third panel to playfield. also set the layout from it.

--- src/Playfield.java
+++ src/Playfield.java
@@ -1,6 +1,7 @@
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
 
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -9,14 +10,24 @@ import javax.swing.JTextField;
 public class Playfield extends JPanel{
 	
 	private static final long serialVersionUID = 1L;
-	JPanel panelCreditPrize = new JPanel();
+	JPanel panelCreditPrize, panelSlotMachine, panelStartStopBtn;
 	JLabel credit, prize;
 	JTextField creditText, prizeText;
+	
+	JButton startBtn, stopBtn;
 
 	Playfield(){
+		panelCreditPrize = new JPanel();
+		panelSlotMachine = new JPanel();
+		panelStartStopBtn = new JPanel();
+		
+		this.setLayout( new BorderLayout() );
+		
+		//First Panel - Credits, Prize
 		panelCreditPrize.setLayout( new GridLayout(2,2) );
 		
 		this.credit = new JLabel("Credit");
+		this.credit.setFont(this.credit.getFont().deriveFont(60));
 		this.prize = new JLabel("Prize");
 		
 		this.creditText = new JTextField("100");
@@ -27,7 +38,20 @@ public class Playfield extends JPanel{
 		this.panelCreditPrize.add(this.creditText);
 		this.panelCreditPrize.add(this.prizeText);
 		
-		this.add(this.panelCreditPrize);
+		//Second Panel - slot machine
+		
+		//Third Panel - start/stop buttons
+		this.startBtn = new JButton("start");
+		this.stopBtn = new JButton("stop");
+		
+		panelStartStopBtn.add(this.startBtn);
+		panelStartStopBtn.add(this.stopBtn);
+		
+		
+		//add the three panels to playfield
+		this.add(this.panelCreditPrize, BorderLayout.NORTH);
+		this.add(this.panelSlotMachine, BorderLayout.CENTER);
+		this.add(this.panelStartStopBtn, BorderLayout.SOUTH);
 		
 	}
 
